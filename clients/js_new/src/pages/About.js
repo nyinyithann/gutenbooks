b36ffd6cc5e9cc471e44e79512a73139d68da167
Nyi Nyi <nyinyithann@gmail.com>
2022-10-02 22:32:23 +0800

    modified about page

--- clients/js_new/src/pages/About.js
+++ clients/js_new/src/pages/About.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 function AuthorLink({ link, site, title }) {
   return (
-    <div>
-      <span className="text-600"> {`${site}:`} </span>
+    <div className="flex justify-start items-center">
+      <span className="flex-shrink-0 w-[4.5rem] text-600 text-left justify-start items-center p-[1px] px-2 m-[2px] bg-50 rounded-r-full">
+        {site}
+      </span>
       <a
         href={link}
         target="blank"
-        className="underline hover:cursor-pointer hover:text-600 text-400 pl-2"
+        className="underline hover:cursor-pointer hover:text-600 text-500 pl-2"
       >
         {title}
       </a>
@@ -19,44 +21,52 @@ function About() {
   return (
     <div className="flex flex-col gap-2 text-600 font-mono text-center h-full items-center justify-center">
       <div className="-mt-40">
-        <h1 className="text-xl">Gutenbook</h1>
+        <h1 className="text-2xl text-900 font-bold font-sans">Gutenbooks</h1>
         <div className="pt-4">
           <a
             target="_blank"
             rel="noreferrer"
-            className="text-800 underline text-base md:text:xl text-center inline"
+            className="text-800 underline text-xl text-center inline font-sans"
             href="https://github.com/nyinyithann/gutenbooks"
           >
-            Gutenbook GitHub Reposistory
+            Gutenbooks GitHub Reposistory
           </a>
         </div>
-        <div className="pt-2 font-sans text-sm md:text-base">
+        <div className="pt-2 font-sans text-xl">
           <div className="flex flex-col justify-center items-center font-sans font-normal text-[0.7em] mt-4 pt-2 border-t border-200">
-            <div className="w-24 h-24 relative">
+            <div className="w-24 h-24 relative mb-4">
               <img
                 src="https://avatars.githubusercontent.com/u/156037"
                 alt="mygithub"
-                className="w-full h-full rounded-full border-4 border-300"
+                className="w-full h-full rounded-full border-4 border-300 sepia-0 saturate-150"
               />
               <span className="bg-900 rounded-full text-xs text-200 p-1 absolute right-1 -top-1 border-2 border-300">
                 By{' '}
               </span>
             </div>
-            <AuthorLink
-              link="https://github.com/nyinyithann"
-              title="@nyinyithann"
-              site="GitHub"
-            />
-            <AuthorLink
-              link="https://www.linkedin.com/in/nyinyithan/"
-              title="@nyinyithann"
-              site="LinkedIn"
-            />
-            <AuthorLink
-              link="https://twitter.com/JazzTuyat"
-              title="@JazzTuyat"
-              site="Twitter"
-            />
+            <div>
+              <AuthorLink
+                link="https://github.com/nyinyithann"
+                title="@nyinyithann"
+                site="GitHub"
+              />
+              <AuthorLink
+                link="https://www.linkedin.com/in/nyinyithan/"
+                title="@nyinyithann"
+                site="LinkedIn"
+              />
+              <AuthorLink
+                link="https://twitter.com/JazzTuyat"
+                title="@JazzTuyat"
+                site="Twitter"
+              />
+
+              <AuthorLink
+                link="https://nyinyithan.com"
+                title="https://nyinyithan.com"
+                site="Blog"
+              />
+            </div>
           </div>
         </div>
       </div>
